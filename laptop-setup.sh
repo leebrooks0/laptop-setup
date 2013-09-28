f423da9f42c194860d6a2e8a7af841f2423eafdb
lee <leebrooks0@gmail.com>
2013-09-28 15:31:46 +0200

    New changes

--- laptop-setup.sh
+++ laptop-setup.sh
@@ -4,11 +4,10 @@ sudo apt-get update
 # Install curl
 sudo apt-get install curl -y
 
-# Install RVM, latest Ruby, latest Rails and all dependencies
-\curl -L https://get.rvm.io | bash -s stable --rails
-# Need to first set shell to 'Login Shell' 
+# Install RVM, latest Ruby, and all dependencies
+\curl -L https://get.rvm.io | bash -s stable --ruby
+# Need to first set shell to 'Login Shell'
 # https://rvm.io/integration/gnome-terminal
-rvm --default use 2.0.0-p195
 gem install rails
 gem install bundler
 
@@ -16,22 +15,39 @@ gem install bundler
 sudo apt-get install postgresql -y
 sudo apt-get install libpq-dev -y # To install pg gem
 sudo apt-get install pgadmin3 -y
-sudo -u postgres createuser --superuser $USER -y
 
 # Install NodeJS
+sudo add-apt-repository ppa:chris-lea/node.js -y
+sudo apt-get update
 sudo apt-get install nodejs -y
 
-# Install Oracle JDK for RubyMine 
-# http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
-sudo add-apt-repository ppa:webupd8team/java -y
+# Install Redis
+sudo add-apt-repository ppa:chris-lea/redis-server -y
 sudo apt-get update
-sudo apt-get install oracle-java8-installer -y 
+sudo apt-get install redis-server -y
+
+# Install VIM
+sudo add-apt-repository ppa:nmi/vim-snapshots -y
+sudo apt-get update
+sudo apt-get install gvim -y
+
+# Install CTAGS
+sudo apt-get install exuberant-ctags -y
+
+# Install the Silver Searcher
+sudo apt-get install silversearcher-ag -y
+
+# Install Oracle JDK for RubyMine
+# http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
+# sudo add-apt-repository ppa:webupd8team/java -y
+# sudo apt-get update
+# sudo apt-get install oracle-java8-installer -y
 
 # Install Git
 sudo apt-get install git -y
 
 # Install ImageMagick
-sudo apt-get install imagemagick --fix-missing -y
+sudo apt-get install imagemagick -y
 
 # Install Terminator
 sudo apt-get install terminator -y
@@ -51,6 +67,9 @@ sudo apt-get install shutter -y
 sudo apt-get install searchmonkey -y
 sudo apt-get install unetbootin -y
 sudo apt-get install wine -y
+
+sudo apt-get install mendeleydesktop -y
+
 
 # Add standard directories
 cd ~
